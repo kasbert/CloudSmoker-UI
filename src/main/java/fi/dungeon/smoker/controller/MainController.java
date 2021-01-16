44f56ce255384a3a0adb312eb20f97babec71d44
Jarkko Sonninen <kasper@iki.fi>
2021-01-16 20:13:01 +0200

    Style fixes

--- src/main/java/fi/dungeon/smoker/controller/MainController.java
+++ src/main/java/fi/dungeon/smoker/controller/MainController.java
@@ -1,5 +1,6 @@
 package fi.dungeon.smoker.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
